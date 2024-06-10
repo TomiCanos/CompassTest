90c4832f416d3b92c4f8d274dc16ae30435f9bc6
tomas canosa <tomascanosa@MacBook-Pro-de-tomas.local>
2024-06-10 20:45:55 -0300

    separate composables and add previews

---
+++ app/src/main/java/com/example/compasstest/ui/Footer.kt
@@ -0,0 +1,26 @@
+package com.example.compasstest.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Footer(onClick: () -> Unit) {
+    Column {
+        Divider()
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            onClick = onClick,
+        ) {
+            Text("Show every tenth character")
+        }
+    }
+}
--- app/src/main/java/com/example/compasstest/ui/MainActivity.kt
+++ app/src/main/java/com/example/compasstest/ui/MainActivity.kt
@@ -4,22 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
-import com.example.compasstest.ui.theme.CompassTestTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -29,42 +15,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent { MainContent() }
-    }
-
-    @Composable
-    fun MainContent() {
-        val everyTenthCharacterResponse by viewModel.response.collectAsState()
-
-        CompassTestTheme {
-            Column(modifier = Modifier.fillMaxSize()) {
-                val scrollState = rememberScrollState()
-
-                Text(
-                    text = everyTenthCharacterResponse,
-                    modifier = Modifier
-                        .weight(1f)
-                        .verticalScroll(scrollState)
-                        .padding(16.dp)
-                )
-
-                Footer()
-            }
-        }
-    }
-
-    @Composable
-    fun Footer() {
-        Column {
-            Divider()
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-                onClick = { viewModel.getEveryTenthCharacter() },
-            ) {
-                Text("Show every tenth character")
-            }
+        setContent {
+            val everyTenthCharacterResponse by viewModel.response.collectAsState()
+            MainContent(
+                firstText = everyTenthCharacterResponse,
+                onFooterClick = { viewModel.getEveryTenthCharacter() }
+            )
         }
     }
 }
