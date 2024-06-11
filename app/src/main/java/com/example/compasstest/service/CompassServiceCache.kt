7ed5cd3913c124f1b8af03c6b350a8f1598bda93
tomas canosa <tomascanosa@MacBook-Pro-de-tomas.local>
2024-06-11 00:50:35 -0300

    basic structure for a service cache

---
+++ app/src/main/java/com/example/compasstest/service/CompassServiceCache.kt
@@ -0,0 +1,13 @@
+package com.example.compasstest.service
+
+internal object CompassServiceCache {
+
+    fun saveAboutResponse(response: String) {
+        //write to local storage
+    }
+
+    fun getAboutResponse(): String? {
+        //read from local storage
+        return ""
+    }
+}
